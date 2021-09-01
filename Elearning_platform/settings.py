ee9b26cee0f829ca7ebc19b03aa52cff24964623
Adrian <golinski.adrian@gmail.com>
2021-09-01 12:44:17 +0200

    still learning about django. Remove secret key from settings.py

--- Elearning_platform/settings.py
+++ Elearning_platform/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from django.urls import reverse_lazy
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3%w(t+yizagf=0%3eg3&caklwp22ugj_yj)kw&0#tmri0dgk!)'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
